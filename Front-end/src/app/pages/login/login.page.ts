e85fcd400a01c2b6f21889039625ae47a75dd9c0
Omar Malaeb <omarmlb002@gmail.com>
2022-05-21 12:01:07 +0300

    designed login page

--- Front-end/src/app/pages/login/login.page.ts
+++ Front-end/src/app/pages/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private service: UserService, private alertController: AlertController) {
+
+  }
 
   ngOnInit() {
+
+  }
+
+  async showAlertFail(){
+    const alert = await this.alertController.create({
+      header: 'Login Failed',
+      message: 'Email or password is incorrect. Please try again',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  async showAlertFailEmail(){
+    const alert = await this.alertController.create({
+      header: 'Email address required',
+      message: 'Please enter a valid email address',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  async showAlertFailPassword(){
+    const alert = await this.alertController.create({
+      header: 'Password required',
+      message: 'Please enter a password',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  public onLogin(form : NgForm){
+  
   }
 
 }
