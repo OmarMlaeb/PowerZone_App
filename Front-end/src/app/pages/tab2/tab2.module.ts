8829ae507db9874c7d5de7216518ee1aad7b646a
Omar Malaeb <omarmlb002@gmail.com>
2022-05-24 14:03:10 +0300

    added calender in tab2 for events

--- Front-end/src/app/pages/tab2/tab2.module.ts
+++ Front-end/src/app/pages/tab2/tab2.module.ts
@@ -1,20 +1,24 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { NgCalendarModule  } from 'ionic2-calendar';
 
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+registerLocaleData(localeDe);
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
+    NgCalendarModule
   ],
   declarations: [Tab2Page]
 })
