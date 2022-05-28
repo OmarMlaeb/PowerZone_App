43733315aef7ecb93655f07a8179287781a80739
Omar Malaeb <omarmlb002@gmail.com>
2022-05-28 14:44:45 +0300

    implemented tab 3

--- Front-end/src/app/pages/tab3/tab3.page.ts
+++ Front-end/src/app/pages/tab3/tab3.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { FeesService, Fee, monthlyFee } from '../../services/fees.service';
+import { AttendancePage } from '../attendance/attendance.page';
+import { TotalFeesPage } from '../total-fees/total-fees.page';
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -17,5 +20,54 @@ export class Tab3Page {
   fees: Fee[];
   total: number[];
   monthly: monthlyFee[];
+
+  ngOnInit() {
+    this.service.getFees().subscribe(response => {
+      this.fees = response;
+      console.log(this.fees);
+      this.total= [this.fees[this.fees.length-1]["SUM(Amount)"]];
+    })
+    
+    this.service.getMonthlyFees().subscribe(response => {
+      this.monthly = response;
+    })
+  }
+
+  async initModal() {
+    const modal = await this.modalCtrl.create({
+      component: TotalFeesPage,
+      componentProps: {
+        'fee':this.monthly[0]["total"],
+        'month': this.monthly[0]["month"], 
+        'year':  this.monthly[0]["year"]
+      }
+    });
+
+    modal.onDidDismiss().then((modalDataResponse) => {
+      if (modalDataResponse !== null) {
+        this.modalDataResponse = modalDataResponse.data;
+        console.log('Modal Sent Data: ' + modalDataResponse.data);
+      }
+    });
+
+    return await modal.present();
+  }
+
+  async initAttendanceModal() {
+    const modal = await this.modalCtrl.create({
+      component: AttendancePage,
+      componentProps: {
+      }
+    });
+
+    modal.onDidDismiss().then((modalDataResponse) => {
+      if (modalDataResponse !== null) {
+        this.modalDataResponse = modalDataResponse.data;
+        console.log('Modal Sent Data : ' + modalDataResponse.data);
+      }
+    });
+
+    return await modal.present();
+  }
 
 }
